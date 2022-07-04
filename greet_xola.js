393d97991ba732d6a0d56d1e0ace54ac77fe55fe
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-07-04 17:05:51 +0200

    first node project

--- greet_xola.js
+++ greet_xola.js
@@ -1,7 +1,17 @@
 import chalk from "chalk"
 
+import figlet from "figlet"
+
 import greet from "./greet.js"
 
 let msg1 = chalk.bgGreen.black(greet('Xola'))
 
 console.log(msg1)
+
+console.log(figlet(greet("Lukhanyo"), function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        return;
+    }
+    console.log(data)
+}))
